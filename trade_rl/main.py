832176c725a88191a144e75b39a5730a1f22ae49
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-28 11:09:46 -0400

    Add dummy config

--- trade_rl/main.py
+++ trade_rl/main.py
@@ -21,7 +21,7 @@ parser.add_argument(
 def main() -> None:
     args = parser.parse_args()
     config_file_path = get_root_dir() / args.config_file
-    meta_args, _ = parse_args(config_file_path)
+    meta_args, *_ = parse_args(config_file_path)
     setup_basic_logging(meta_args.log_file_path)
     seed_everything(meta_args.global_seed)
     run()
