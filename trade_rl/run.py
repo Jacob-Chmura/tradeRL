85d3b86ba293be2fe100cffff01fa6535d900de0
Jacob Chmura <jacobpaul.chmura@gmail.com>
2025-03-28 21:24:45 -0400

    Add Env (#30)

--- trade_rl/run.py
+++ trade_rl/run.py
@@ -1,5 +1,25 @@
 import logging
 
+import gymnasium as gym
+from tqdm import tqdm
+
+from trade_rl.order import Order
+
 
 def run() -> None:
-    logging.info('TODO')
+    order = Order(sym='TSLA', qty=100, end_time=1337)
+    logging.info(f'Order: {order}')
+
+    env = gym.make('trade_rl/TradingEnvironment-v0', order=order)
+    logging.info(f'Created Environment')
+
+    n_episodes = 100
+    for episode in tqdm(range(n_episodes)):
+        obs, info = env.reset()
+        done = False
+
+        while not done:
+            action = 0
+            next_obs, reward, terminated, truncated, info = env.step(action)
+
+            done = terminated or truncated
