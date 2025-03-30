a9f272cc0438b80b75c071e44615d18ab2c31b42
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-30 13:28:45 -0400

    Setup basic order generator

--- trade_rl/env.py
+++ trade_rl/env.py
@@ -1,30 +1,36 @@
+import logging
 from typing import Any, Dict, Optional, Tuple
 
 import gymnasium as gym
 
-from trade_rl.order import Order
+from trade_rl.order import OrderGenerator
 
 
 class TradingEnvironment(gym.Env):
-    def __init__(self, order: Order) -> None:
+    def __init__(self) -> None:
         self.action_space = gym.spaces.Discrete(2)  # Skip or Take
         self.observation_space = gym.spaces.Discrete(1)  # TODO
+        logging.info(f'Created Environment')
 
-        self._order = order
         self._step = 0
+        self._order_generator = OrderGenerator()
+        self.order = self._order_generator()
+        logging.info(f'New order: {self.order}')
 
     def reset(
         self, seed: Optional[int] = None, options: Optional[dict] = None
     ) -> Tuple[Any, ...]:
         super().reset(seed=seed)
         self._step = 0
+        self.order = self._order_generator()
+        logging.info(f'New order: {self.order}')
 
         obs = self._get_obs()
         info = self._get_info()
         return obs, info
 
     def step(self, action: int) -> Tuple[Any, ...]:
-        terminated = self._step >= self._order.end_time
+        terminated = self._step >= self.order.end_time
         truncated = False
         reward = 1 if terminated else 0
         obs = self._get_obs()
--- trade_rl/order.py
+++ trade_rl/order.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import random
 from dataclasses import dataclass
 from typing import Literal
 
@@ -7,3 +10,17 @@ class Order:
     sym: Literal['TSLA']
     qty: int
     end_time: int
+
+
+class OrderGenerator:
+    def __init__(self) -> None:
+        self.sym_spec = ['TSLA']
+        self.qty_spec = [10, 50]
+        self.end_time_spec = [100, 1000]
+
+    def __call__(self) -> Order:
+        return Order(
+            sym=random.choice(self.sym_spec),  # type: ignore
+            qty=random.choice(self.qty_spec),
+            end_time=random.choice(self.end_time_spec),
+        )
