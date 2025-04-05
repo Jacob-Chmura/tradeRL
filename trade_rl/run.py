31bcdfaecad89948549ef4ee75ac4e7a78a7051c
Jacob Chmura <jacobpaul.chmura@gmail.com>
2025-04-05 19:35:57 -0400

    Boilerplate for testing (#60)
    * Boilerplate for testing
    
    * Update spelling
    
    * Update

--- trade_rl/run.py
+++ trade_rl/run.py
@@ -10,14 +10,32 @@ def run(args: Args) -> None:
     data = Data('data/TSLA/OCHLV')
     env = TradingEnvironment(args, data)
     agent = agent_from_env(env, agent_type='random')
-    with tqdm(total=args.env.max_global_steps) as pbar:
-        while env.global_step < args.env.max_global_steps:
+    with tqdm(total=args.env.max_train_steps) as pbar:
+        while env.global_step < args.env.max_train_steps:
             obs, info = env.reset()
             done = False
             while not done:
                 action = agent.get_action(obs)
                 next_obs, reward, terminated, truncated, info = env.step(action)
                 agent.update(obs, action, reward, terminated, next_obs)
+                done = terminated or truncated
+                obs = next_obs
+
+                pbar.update(1)
+
+    # TODO: Make sure agent is in 'eval' mode
+    # TODO: Ensure perf tracker is aware of the fact we are in eval mode
+    # TODO: Load agent model state dict
+    # TODO: Configure environment (data file) to different train/test
+    env = TradingEnvironment(args, data)
+    agent = agent_from_env(env, agent_type='random')
+    with tqdm(total=args.env.max_test_steps) as pbar:
+        while env.global_step < args.env.max_test_steps:
+            obs, info = env.reset()
+            done = False
+            while not done:
+                action = agent.get_action(obs)
+                next_obs, reward, terminated, truncated, info = env.step(action)
                 done = terminated or truncated
                 obs = next_obs
 
