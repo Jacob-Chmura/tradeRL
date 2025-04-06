1ddaf0a834a0e4b2970d9ec2bfb2b9b330899266
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-04-06 14:35:19 -0400

    WIP

--- trade_rl/util/data.py
+++ trade_rl/util/data.py
@@ -1,6 +1,4 @@
-import glob
 import logging
-import os
 import random
 from typing import Tuple
 
@@ -9,18 +7,11 @@ import pandas as pd
 
 class Data:
     def __init__(self, data_path: str) -> None:
-        file_pattern = os.path.join(data_path, '*.parquet')
-        files = glob.glob(file_pattern)
-        if not files:
-            raise ValueError('Data not found')
-
-        logging.debug(f'Reading raw data files: {files}')
-        self.data = pd.concat([pd.read_parquet(file) for file in files])
+        logging.debug(f'Reading raw data from: {data_path}')
+        self.data = pd.read_parquet(data_path)
         logging.debug(
             f'Read {self.data.memory_usage(deep=True).sum() / 1e9} GB ({len(self.data)} rows)'
         )
-        self.data['ts_event'] = pd.to_datetime(self.data['ts_event'])
-        self.data['date'] = self.data['ts_event'].dt.date
         self.unique_days = self.data['date'].unique()
 
     def get_order_data(
@@ -40,5 +31,4 @@ class Data:
             end_time = available_rows - start_time
 
         data_chunk = day_data.iloc[: start_time + end_time]
-
         return data_chunk, start_time - 1, end_time
