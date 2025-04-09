b2f0404058582b43d7a735ca79f402079ab268af
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-04-09 10:34:10 -0400

    refactor

--- trade_rl/agents/baselines.py
+++ trade_rl/agents/baselines.py
@@ -11,53 +11,24 @@ class RandomAgent(TradingAgent):
 
 class BuyStartAgent(TradingAgent):
     def get_action(self, obs: Any) -> int:
-        current_step = self.env.episode_step
-        orderQuantity = self.env.order.qty
-
-        # The agent will buy shares at the start of the episode
-        if current_step < orderQuantity:
-            self.logger.info(f'BUY a share at start step {current_step}')
-            return 1
-
-        # Skip if the order is completed
-        else:
-            self.logger.info(f'Order complete: SKIP at step {current_step}')
-            return 0
+        return 1
 
 
 class BuyLastAgent(TradingAgent):
     def get_action(self, obs: Any) -> int:
-        current_step = self.env.episode_step
-        orderQuantity = self.env.order.qty
-        finalTimeStep = self.env.order.end_time
-
         # The agent will buy shares at the end of the episode
-        if current_step >= finalTimeStep - orderQuantity:
-            self.logger.info(f'BUY a share at last step {current_step}')
+        if self.env.episode_step >= self.env.order.end_time - self.env.order.qty:
+            self.logger.info(f'BUY: step {self.env.episode_step}')
             return 1
-
-        # Skip if the order is completed
-        else:
-            self.logger.info(f'Not the end: SKIP at step {current_step}')
-            return 0
+        return 0
 
 
-class LinearAgent(TradingAgent):
+class BuyLinearScheduleAgent(TradingAgent):
     def get_action(self, obs: Any) -> int:
-        # Skip if the order is completed
-        if self.env.remaining_qty <= 0:
-            self.logger.info(f'Order complete: SKIP at step {self.env.episode_step}')
-            return 0
-
-        current_step = self.env.episode_step
-        orderQuantity = self.env.order.qty
-        finalTimeStep = self.env.order.end_time
-
-        interval = finalTimeStep // orderQuantity
-
         # The agent will buy shares linearly
-        if current_step % interval == 0:
-            self.logger.info(f'BUY a share at linear interval at step {current_step}')
+        interval = self.env.order.end_time // self.env.order.qty
+        if self.env.episode_step % interval == 0:
+            self.logger.info(f'BUY: step {self.env.episode_step}')
             return 1
         return 0
 
@@ -65,7 +36,7 @@ class LinearAgent(TradingAgent):
 class BuyBelowArrivalAgent(TradingAgent):
     def __init__(self, env: TradingEnvironment) -> None:
         super().__init__(env)
-        self.arrival_price = 0.0
+        # self.arrival_price = 0.0
 
     def get_action(self, obs: Any) -> int:
         # At first step, the agent will set the arrival price
@@ -73,16 +44,8 @@ class BuyBelowArrivalAgent(TradingAgent):
             self.arrival_price = self.env.order_data['open'][self.env.start_index]
             return 0
 
-        # Skip if the order is completed
-        if self.env.remaining_qty <= 0:
-            self.logger.info(f'Order complete: SKIP at step {self.env.episode_step}')
-            return 0
-
-        current_index = self.env.start_index + self.env.episode_step
-        price = self.env.order_data['open'][current_index]
-        if price < self.arrival_price:
-            self.logger.info(f'BUY, price below arrival step {self.env.episode_step}')
+        px = self.env.order_data['open'][self.env.start_index + self.env.episode_step]
+        if px < self.arrival_price:
+            self.logger.info(f'BUY: step {self.env.episode_step}')
             return 1
-        else:
-            self.logger.info(f'SKIP, price above arrival step {self.env.episode_step}')
-            return 0
+        return 0
