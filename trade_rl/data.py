4c567a8c070e66b4ca68f3e0e5716dc35248a3aa
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-04-09 11:44:21 -0400

    WIP

--- trade_rl/data.py
+++ trade_rl/data.py
@@ -26,12 +26,9 @@ class Data:
         self, start_time: int, max_steps: int
     ) -> Tuple[pd.DataFrame, int, int]:
         date = random.choice(self.unique_days)
-        max_end_time = start_time + max_steps
-        mask = (self.data.date == date) & (self.data.market_second <= max_end_time)
-        data = self.data[mask].copy()
-
+        data = self.data[self.data.date == date].copy()
         order_start_index = len(data[data.market_second < start_time])
-        max_steps = len(data) - order_start_index - 1  # TODO: 'Fill in' with data
+        max_steps = min(max_steps, len(data) - order_start_index - 1)
         return data, order_start_index, max_steps
 
 
