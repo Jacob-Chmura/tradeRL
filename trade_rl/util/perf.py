730591e5a1e7aed6b223eb30bc89549b8cb00c92
Jacob Chmura <jacobpaul.chmura@gmail.com>
2025-04-10 16:07:13 -0400

    TQDM Stats (#107)
    * WIP
    
    * WIp

--- trade_rl/util/perf.py
+++ trade_rl/util/perf.py
@@ -9,6 +9,8 @@ from trade_rl.util.path import get_root_dir, get_run_id
 
 
 class PerfTracker:
+    BAR_FORMAT = '{desc}|{bar:20}| {n_fmt}/{total_fmt}, {rate_fmt}'
+
     def __init__(self, fields: List[str], args: Args) -> None:
         self.log_dir = get_root_dir() / 'runs' / get_run_id(args.meta.experiment_name)
         self.log_dir.mkdir(parents=True, exist_ok=True)
@@ -25,6 +27,14 @@ class PerfTracker:
             self.writers[mode] = csv.DictWriter(fp, fields, lineterminator='\n')
             self.writers[mode].writeheader()
 
+        self.stats: Dict[str, Any] = {
+            'episode': 0,
+            'symbol': '',
+            'vwap': OnlineEMA(),
+            'ap': OnlineEMA(),
+            'urgency': OnlineEMA(),
+            'reward': OnlineEMA(),
+        }
         self.train()
 
     def __del__(self) -> None:
@@ -35,8 +45,52 @@ class PerfTracker:
         info['time'] = time.time_ns()
         self.writers[self.mode].writerow(info)
 
+        self.stats['episode'] = info['episode']
+        self.stats['symbol'] = info['order_symbol']
+        self.stats['vwap'].update(info['vwap_slippage'])
+        self.stats['ap'].update(info['arrival_slippage'])
+        self.stats['urgency'].update(100 * (1 - info['step'] / info['order_duration']))
+        self.stats['reward'].update(info['total_reward'])
+
     def train(self) -> None:
         self.mode = 'train'
 
     def eval(self) -> None:
         self.mode = 'eval'
+
+    def pretty(self) -> str:
+        symbol = self.stats['symbol']
+        episode = self.stats['episode']
+        ap = self.stats['ap'].ema
+        vwap = self.stats['vwap'].ema
+        urgency = self.stats['urgency'].ema
+        reward = self.stats['reward'].ema
+        s = ''
+        s += self._color(f'Episode [{str(episode).zfill(6)}]', 'yellow')
+        s += '      '
+        s += self._color(f'VWAP: {vwap:3.2f} bps', 'red' if vwap > 0 else 'green')
+        s += '      '
+        s += self._color(f'AP: {ap:3.2f} bps', 'red' if ap > 0 else 'green')
+        s += '      '
+        s += self._color(f'Reward: {reward:3.2f}', 'cyan')
+        s += '      '
+        s += self._color(symbol, 'black')
+        s += '      '
+        s += self._color(f'Urgency: {urgency:3.2f}%', 'white')
+        s += '      '
+        s += 'Global Step'
+        return s
+
+    @staticmethod
+    def _color(s: str, color: str, background: bool = False) -> str:
+        colors = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+        return f'\u001b[{10 * background + 60 * (color.upper() == color) + 30 + colors.index(color.lower())}m{s}\u001b[0m'
+
+
+class OnlineEMA:
+    def __init__(self, alpha: float = 0.0005) -> None:
+        self.alpha = alpha
+        self.ema = 0.0
+
+    def update(self, x: float) -> None:
+        self.ema = self.alpha * x + (1 - self.alpha) * self.ema
