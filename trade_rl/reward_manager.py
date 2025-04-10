a92b6395dec33cc88a67176ec7e1cd87bf9f47fa
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-04-10 13:49:11 -0400

    Switch to bps

--- trade_rl/reward_manager.py
+++ trade_rl/reward_manager.py
@@ -21,24 +21,25 @@ class RewardManager:
         self.is_sparse = 'sparse' in reward_args.reward_type
         self.slippage_type = reward_type.split('_')[0]  # arrival_sparse -> arrival
         self.terminal_cost_multiplier = reward_args.termination_px_cost_multiplier
-
-        self.arrival_benchmark = lambda: self.env.order_arrival['open']
-        self.vwap_benchmark = lambda: self.env.previous['vwap']
-        self.oracle_benchmark = (
-            lambda: self.env.order_duration_market['vwap']
-            .nsmallest(self.env.info.step)
-            .mean()
-        )
+        self.benchmarks = {
+            'arrival': lambda: self._slippage_bps(self.env.order_arrival['open']),
+            'vwap': lambda: self._slippage_bps(self.env.previous['vwap']),
+            'oracle': lambda: self._slippage_bps(
+                self.env.order_duration_market['open']
+                .nsmallest(self.env.info.step)
+                .mean(),
+            ),
+        }
 
     def __call__(self, order_done: bool) -> Tuple[Dict[str, float], float]:
-        slippages = {'arrival': 0.0, 'vwap': 0.0, 'oracle': 0.0}
-        if len(self.env.info.portfolio):
-            slippages['arrival'] = self.env.info.agent_vwap - self.arrival_benchmark()
-            slippages['vwap'] = self.env.info.agent_vwap - self.vwap_benchmark()
-            slippages['oracle'] = self.env.info.agent_vwap - self.oracle_benchmark()
-
+        slippages = {}
+        for name, benchmark in self.benchmarks.items():
+            slippages[name] = benchmark() if len(self.env.info.portfolio) else 0.0
         cost = 0 if self.is_sparse else slippages[self.slippage_type]
         if order_done and self.env.info.qty_left > 0:
             finish_px = self.terminal_cost_multiplier * self.env.current['high']
             cost += finish_px - self.env.order_arrival['open']
         return slippages, -cost
+
+    def _slippage_bps(self, benchmark_px: float) -> float:
+        return 10_000 * (self.env.info.agent_vwap - benchmark_px) / benchmark_px
